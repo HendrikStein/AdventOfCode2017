32fefe1d3e03845a9e71388a68ec85f3a334e96c
Hendrik Stein <hstein@jastech-solutions.de>
2017-12-01 10:22:48 +0100

    Day 1

---
+++ aoc2017/src/main/java/aoc/day1/Runner.java
@@ -0,0 +1,58 @@
+package aoc.day1;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.StringCharacterIterator;
+
+import org.apache.commons.io.FileUtils;
+
+public class Runner {
+
+	public static void main(String[] args) throws IOException {
+		String input = FileUtils.readFileToString(new File("src//main//resources//day1.input"), "UTF-8");
+
+		task1(input);
+
+		int half = input.length() / 2;
+		task2(input.substring(0, half), input.substring(half, input.length()));
+
+	}
+
+	private static void task2(String list1, String list2) {
+		int sum = 0;
+
+		for (int i = 0; i < list1.length(); i++) {
+			int currentList1 = Character.getNumericValue(list1.charAt(i));
+			int currentList2 = Character.getNumericValue(list2.charAt(i));
+
+			if (currentList1 == currentList2) {
+				sum += currentList1 + currentList2;
+			}
+		}
+
+		System.out.println("Summe Task 2: " + sum);
+	}
+
+	private static void task1(String input) {
+		StringCharacterIterator iterator = new StringCharacterIterator(input);
+		int sum = 0;
+		for (int i = 0; i < input.length(); i++) {
+			if (i + 1 > input.length() - 1) {
+				break;
+			}
+
+			int current = input.charAt(i);
+			int next = input.charAt(i + 1);
+			if (current == next) {
+				sum += Character.getNumericValue(input.charAt(i));
+			}
+		}
+
+		if (iterator.first() == iterator.last()) {
+			sum += Character.getNumericValue(iterator.first());
+		}
+
+		System.out.println("Summe Taks 1: " + sum);
+	}
+
+}
